114857611969c9bb8d458d1e9d4ad699fe828d67
AbhinRustagi <rustagi.abhin@gmail.com>
2024-06-26 23:45:29 +1000

    feat: add footer

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,3 +1,37 @@
+import Link from "next/link";
+
 export default function Footer() {
-  return <footer className="mt-10">footer.</footer>;
+  return (
+    <footer className="mt-10 border-t border-t-gray-300 pt-10">
+      <p className="mb-4">
+        Thanks for stopping by! <br />I would love to hear from you. You can
+        find me on email at{" "}
+        <Link className="underline" href="mailto:hi@abhin.dev">
+          hi@abhin.dev
+        </Link>
+        .
+      </p>
+      <p>
+        You can also socialize with me elsewhere –{" "}
+        <Link
+          className="underline"
+          href="https://www.linkedin.com/in/abhinrustagi"
+        >
+          LinkedIn
+        </Link>
+        ,{" "}
+        <Link className="underline" href="https://www.github.com/AbhinRustagi">
+          Github
+        </Link>{" "}
+        &{" "}
+        <Link
+          className="underline"
+          href="https://www.twitter.com/@abhinrustagi"
+        >
+          Twitter
+        </Link>
+        .
+      </p>
+    </footer>
+  );
 }
