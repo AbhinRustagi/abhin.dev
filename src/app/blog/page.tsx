79f8a69712e3f9f19ae44338930936f90d4d37e9
AbhinRustagi <rustagi.abhin@gmail.com>
2024-06-30 23:53:23 +1000

    feat: update blog page desc

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,8 @@
+import ExtLink from "@/components/ExtLink";
 import PostsGrid from "@/components/PostsGrid";
 import { getAllPosts, groupPostsByDate } from "@/lib/blog";
 import { generateMetadata } from "@/lib/metadata";
+import Link from "next/link";
 
 export const metadata = generateMetadata({ title: "Blog" });
 
@@ -11,7 +13,12 @@ export default async function Page() {
   return (
     <section>
       <p className="mb-8">
-        I occasionally pen down my thoughts on tech, and things I learn.
+        I occasionally pen down my thoughts on tech, and things I learn. All my
+        shortform writing is hosted in a{" "}
+        <Link href="https://www.github.com/AbhinRustagi/blog">
+          Github Repository
+        </Link>
+        .
       </p>
       <PostsGrid organisedPosts={organizedPosts} />
     </section>
