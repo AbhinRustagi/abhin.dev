6a94df0ad92f980d1ff5ad870bef88c6ce240501
AbhinRustagi <rustagi.abhin@gmail.com>
2025-02-02 21:34:52 +0530

    feat: finalize theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,11 +15,19 @@ const config: Config = {
     extend: {
       colors: {
         background: "var(--background)",
+        title: "var(--title)",
+        subtitle: "var(--subtitle)",
+        body: "var(--body)",
         accent: "var(--accent)",
         card: "var(--card)",
+        border: "var(--border)",
+        button: "var(--button)",
       },
       backgroundImage: {
         ytm: "linear-gradient(-45deg, rgba(255,8,68,0.45) 0%, rgba(255,117,140,0) 100%)",
+        // buttonGradient:
+        //   "linear-gradient(to right, #f78ca0 0%, #f9748f 19%, #fd868c 60%, #fe9a8b 100%);",
+        buttonGradient: "linear-gradient(60deg, #96deda 0%, #50c9c3 100%);",
       },
     },
   },
